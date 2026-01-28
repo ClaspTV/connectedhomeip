6cfd556c2b52a5128d4ec80f0d71ff307c4b0c35
Radhakrishna <radhakrishna@vizbee.tv>
2026-01-28 11:39:11 +0530

    make periodic discovery example a separate fragment, fix missing R with new 1.4.2 branch, comment out reading basic cluster that causes failures

--- examples/tv-casting-app/android/App/app/src/main/java/com/matter/casting/EndpointSelectorExample.java
+++ examples/tv-casting-app/android/App/app/src/main/java/com/matter/casting/EndpointSelectorExample.java
@@ -3,15 +3,13 @@ package com.matter.casting;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.widget.TextView;
 
-import com.chip.casting.R;
+import com.R;
 import com.matter.casting.core.CastingPlayer;
 import com.matter.casting.core.Endpoint;
 import java.util.List;
 
 import chip.devicecontroller.ChipClusters;
-import chip.devicecontroller.ChipStructs;
 
 /** A utility that selects an endpoint based on some criterion */
 public class EndpointSelectorExample {
@@ -135,30 +133,30 @@ public class EndpointSelectorExample {
 //          });
 //
 //          // read application name: This is working. We are able to see that the application name is com.example.contentapp for endpoint 4. For the rest of the endpoints, we it results in a failure.
-            basicCluster.readApplicationNameAttribute(new ChipClusters.CharStringAttributeCallback() {
-                @Override
-                public void onSuccess(String value) {
-                Log.d(TAG, "ReadApplicationName success. Value: " + value);
-                new Handler(Looper.getMainLooper())
-                        .post(
-                                () -> {
-                                    Log.d(TAG, "applicationName of endpoint: Endpoint: " + endPoint.getId() + ", " +
-                                            " VendorId: " + endPoint.getVendorId() + ", " +
-                                            " ProductId: " + endPoint.getProductId() + " is " + value);
-                                });
-                }
-
-                @Override
-                public void onError(Exception error) {
-                Log.e(TAG, "ReadApplicationName failure " + error);
-                new Handler(Looper.getMainLooper())
-                        .post(() -> {
-                            Log.e(TAG, "Failed to read applicationName of endpoint: Endpoint: " + endPoint.getId() + ", " +
-                                    " VendorId: " + endPoint.getVendorId() + ", " +
-                                    " ProductId: " + endPoint.getProductId());
-                        });
-                }
-            });
+//            basicCluster.readApplicationNameAttribute(new ChipClusters.CharStringAttributeCallback() {
+//                @Override
+//                public void onSuccess(String value) {
+//                Log.d(TAG, "ReadApplicationName success. Value: " + value);
+//                new Handler(Looper.getMainLooper())
+//                        .post(
+//                                () -> {
+//                                    Log.d(TAG, "applicationName of endpoint: Endpoint: " + endPoint.getId() + ", " +
+//                                            " VendorId: " + endPoint.getVendorId() + ", " +
+//                                            " ProductId: " + endPoint.getProductId() + " is " + value);
+//                                });
+//                }
+//
+//                @Override
+//                public void onError(Exception error) {
+//                Log.e(TAG, "ReadApplicationName failure " + error);
+//                new Handler(Looper.getMainLooper())
+//                        .post(() -> {
+//                            Log.e(TAG, "Failed to read applicationName of endpoint: Endpoint: " + endPoint.getId() + ", " +
+//                                    " VendorId: " + endPoint.getVendorId() + ", " +
+//                                    " ProductId: " + endPoint.getProductId());
+//                        });
+//                }
+//            });
         } else {
             Log.e(TAG, "Could not get ApplicationBasicCluster for endpoint with ID: " + endPoint.getId());
         }
