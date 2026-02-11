07c2fe04ab8c89df1f740ed4168dad0307b6df74
Radhakrishna <radhakrishna@vizbee.tv>
2026-02-11 13:53:23 +0530

    update CD for iOS sample app, add iOS product ID to project config file, but comment out for now to be uncommented when building iOS lib

--- examples/tv-casting-app/tv-casting-common/include/CHIPProjectAppConfig.h
+++ examples/tv-casting-app/tv-casting-common/include/CHIPProjectAppConfig.h
@@ -63,6 +63,8 @@
  * The Product ID is assigned by the vendor and SHALL be unique for each product within a Vendor ID.
  */
 #define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_ID 0x0001
+// This is the product for iOS
+// #define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_ID 0x0002
 
 #define CHIP_DEVICE_CONFIG_USE_TEST_SETUP_DISCRIMINATOR 0xF00
 
